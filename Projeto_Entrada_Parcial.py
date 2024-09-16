1c918ae262abec04a8f29ecaeadcc8e765b4cf16
Henrique Costa <henrique_mcosta@hotmail.com>
2024-09-16 14:28:23 -0300

    inicio do projeto

--- Projeto_Entrada_Parcial.py
+++ Projeto_Entrada_Parcial.py
@@ -22,6 +22,7 @@ if response.status_code == 200:
     notificacao()
 else:
     notificacao_1()  
+    
 url_1 = "https://brasilapi.com.br/api/banks/v1/{code}"
 response_1 = requests.get(url_1)
 if response_1.status_code == 200:
