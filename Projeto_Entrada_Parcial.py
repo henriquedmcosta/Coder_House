47a8bfe877ddc0a6b292495a934b43d84f08b362
Henrique Costa <henrique_mcosta@hotmail.com>
2024-09-16 14:42:00 -0300

    alteração

--- Projeto_Entrada_Parcial.py
+++ Projeto_Entrada_Parcial.py
@@ -7,14 +7,14 @@ def notificacao():
         title = "Alerta",
         message = "Importação bem sucedida",
         app_name = "API",
-        timeout = 10 )
+        timeout = 0.8 )
 
 def notificacao_1():
     notification.notify(
         title = "Alerta",
         message = "Importação mal sucedida",
         app_name = "API",
-        timeout = 10 )
+        timeout = 0.8 )
 
 url = "https://brasilapi.com.br/api/banks/v1"
 response = requests.get(url)
@@ -30,4 +30,10 @@ if response_1.status_code == 200:
 else:
     notificacao_1()
     
+url_2 = "https://brasilapi.com.br/api/cep/v1/{cep}"
+response_2 = requests.get(url_1)
+if response_2.status_code == 200:
+    notificacao()
+else:
+    notificacao_1()
 
